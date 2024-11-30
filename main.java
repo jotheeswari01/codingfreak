c7c328544087061f08e400134834939146fdfbea
jotheeswari01 <jotheeswarijotheeswari@gmail.com>
2024-11-30 15:03:59 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter input");
+       // int a=scan.nextInt();
+        //System.out.println(a);
+        //String b=scan.next();
+        //System.out.println(b);
+        char c=scan.next().charAt(0);
+        System.out.println(c);
+        
+    }
+}
