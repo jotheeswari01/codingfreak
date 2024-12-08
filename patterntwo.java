c02085a19f3a946d21c62fdef5c8482ee94a549a
jotheeswari01 <jotheeswarijotheeswari@gmail.com>
2024-12-08 15:10:38 +0530

    Add files via upload

---
+++ patterntwo.java
@@ -0,0 +1,13 @@
+class patterntwo {
+    public static void main(String[] args) {
+        int n=4;
+        int i,j;
+        for(i=n;i>=1;i--){
+            for(j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+         
+    }
+}
