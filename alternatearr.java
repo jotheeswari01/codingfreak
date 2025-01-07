5bee0132a5da9c10d106a3e1338105744d29a359
jotheeswari01 <jotheeswarijotheeswari@gmail.com>
2025-01-07 08:38:36 +0530

    Add files via upload

---
+++ alternatearr.java
@@ -0,0 +1,13 @@
+class alternatearr{
+    public static void main(String [] args){
+        int n=4;
+        int []a={1,2,3,4};
+        for(int i=0;i<n;i+=2)
+        {
+            System.out.println(a[i]);
+
+        }
+        
+
+    }
+}
