f7151ad81bcae36880993e140fb76cb5ed40c420
jotheeswari01 <jotheeswarijotheeswari@gmail.com>
2025-01-07 09:35:51 +0530

    Add files via upload

---
+++ findele1.java
@@ -0,0 +1,15 @@
+class findele1{
+    public static void main(String [] args){
+        int n=7;
+        int key=4;
+        int []a={10,20,30,40,50,60,70};
+        for(int i=0;i<n;i++)
+        {
+            if(i==key)
+            System.out.println(a[i]);
+
+        }
+        
+
+    }
+}
